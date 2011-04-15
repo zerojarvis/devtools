cadb133c8b895a049ac86e3dc84ec63a5d55db8d
Gabriel <gabriel@gartz.com.br>
2011-04-15 20:39:28 -0300

    Moved Route::test() to Route_tester::test() - That isn't a good place for implement the test static method

--- classes/devtools/route/tester.php
+++ classes/devtools/route/tester.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die ('No direct script access.');
 /**
- * Kohana 3 Route tester class.  Use it by calling [Route::test]
+ * Kohana 3 Route tester class.  Use it by calling [Route_Tester::test]
  *
  * @package	   Bluehawk/Devtools
  * @author     Michael Peters
@@ -19,6 +19,70 @@ class Devtools_Route_Tester {
 	// The optional expected params from the config
 	public $expected_params = FALSE;
 
+	/**
+	 * Test a URL or an array of URLs to see which routes they match.
+	 *
+	 * If no param is passed it will test the current url:
+	 *
+	 *     // Test the current url
+	 *     echo Route_Tester::test();
+	 * 
+	 * To test on a single url:
+	 *
+	 *     echo Route_Tester::test('some/url/to/test');
+	 *
+	 * To test several urls:
+	 *
+	 *     echo Route_Tester::test(array(
+	 *         'some/url/to/test',
+	 *         'another/url',
+	 *         'guide/api/Class',
+	 *         'guide/media/image.png',
+	 *     ));
+	 *
+	 * You may also pass the route and parameters you expect, by passing each
+	 * url as a key with an array of expected values.
+	 *
+	 *     $urls = array(
+	 *         'guide/media/image.png' = array(
+	 *             'route' => 'docs/media',
+	 *             'controller' => 'userguide',
+	 *             'action' => 'media',
+	 *             'file' => 'image.png',
+	 *         ),
+	 *
+	 *         'blog/5/some-title` = array(
+	 *             'route' => 'blog',
+	 *             'controller' => 'blog',
+	 *             'action' => 'article',
+	 *             'id' => '5',
+	 *             'title' => 'some-title',
+	 *         ),
+	 *     );
+	 *     echo Route_Tester::test($urls);
+	 *
+	 * It's useful to store your array of urls to be tested in a config file,
+	 * for example in `application/config/my-route-tests.php` return an array
+	 * similar to the previous examples then call:
+	 *
+	 *     echo Route_Tester::test(Kohana::config('your-route-tests'));
+	 *
+	 * @author    Michael Peters
+	 * @license   http://creativecommons.org/licenses/by-sa/3.0/
+	 */
+	public static function test($urls = NULL)
+	{
+		// If no url provide, use the current url
+		if ($urls === NULL)
+		{
+			$urls = Request::current()->uri();
+		}
+		return View::factory('devtools/route-test',array(
+			// Get all the tests
+			'tests' => Route_Tester::create_tests($urls),
+		));
+	}
+	
 	/**
 	 * Get an array of Route_Tester objects from the config settings
 	 *
